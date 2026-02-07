af5555b265223d69593edcfcdfd9c581dab14f94
Garin Yaroslav <garinyaroslav13@mail.ru>
2026-02-07 15:31:22 +0300

    created hw

--- hw01-xml-config/src/main/java/ru/otus/hw/config/AppProperties.java
+++ hw01-xml-config/src/main/java/ru/otus/hw/config/AppProperties.java
@@ -2,9 +2,11 @@ package ru.otus.hw.config;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @Data
+@NoArgsConstructor
 public class AppProperties implements TestFileNameProvider {
     private String testFileName;
 }
--- hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -2,10 +2,19 @@ package ru.otus.hw.dao;
 
 import lombok.RequiredArgsConstructor;
 import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
 
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
 
 @RequiredArgsConstructor
 public class CsvQuestionDao implements QuestionDao {
@@ -13,11 +22,27 @@ public class CsvQuestionDao implements QuestionDao {
 
     @Override
     public List<Question> findAll() {
-        // Использовать CsvToBean
-        // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
-        // Использовать QuestionReadException
-        // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
+        try (InputStream inputStream = getClass().getClassLoader()
+                .getResourceAsStream(fileNameProvider.getTestFileName())) {
 
-        return new ArrayList<>();
+            if (inputStream == null) {
+                throw new QuestionReadException("File not found: " + fileNameProvider.getTestFileName());
+            }
+
+            try (Reader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+                CsvToBean<QuestionDto> csvToBean = new CsvToBeanBuilder<QuestionDto>(reader)
+                        .withType(QuestionDto.class)
+                        .withSeparator(';')
+                        .withSkipLines(1)
+                        .build();
+
+                return csvToBean.parse().stream()
+                        .map(QuestionDto::toDomainObject)
+                        .collect(Collectors.toList());
+            }
+
+        } catch (Exception e) {
+            throw new QuestionReadException("Error reading questions from CSV", e);
+        }
     }
 }
